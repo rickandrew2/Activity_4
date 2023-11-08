6d515ff1727b9295fca3f660be7122e1541289d7
AdrnKen <kenadriane1202@gmail.com>
2023-11-08 12:02:46 +0800

    feat/list-animal-w-most-appoinment

--- veterinary.sql
+++ veterinary.sql
@@ -193,3 +193,11 @@ WHERE animalid IN (
     )
 );
 
+-- feat/list-animal-w-most-appoinment
+SELECT a.animalid, a.name, COUNT(*) AS appointment_count
+FROM animals a
+JOIN appointments ap 
+	ON a.animalid = ap.animalid
+GROUP BY a.animalid, a.name
+ORDER BY appointment_count DESC
+LIMIT 1;
