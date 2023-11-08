8a4f9e49ee5b4b66a4c9a34a6682672175b059a7
rickandrew2 <rickandrew.macapagal@gmail.com>
2023-11-08 10:03:47 +0700

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,2 +1,12 @@
 --feat/create-database-veterinary
 CREATE DATABASE veterinary;
+
+--feat/create-table-owners
+CREATE TABLE owners(
+    ownerid INT PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100),
+);
