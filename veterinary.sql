8de4b273945f17ccd51aba36b24416ae08a8d1e9
AdrnKen <kenadriane1202@gmail.com>
2023-11-08 11:09:37 +0800

    feat/create-table-doctors

--- veterinary.sql
+++ veterinary.sql
@@ -32,3 +32,13 @@ CREATE TABLE appointments (
     reason VARCHAR(255),
     FOREIGN KEY (animalid) REFERENCES animals(animalid)
 );
+
+--feat/create-table-doctors
+CREATE TABLE doctors(
+	doctorid INT PRIMARY KEY, 
+	dfirstname VARCHAR(50),
+	dlastname VARCHAR(50),
+	speciality VARCHAR(50), 
+	phone VARCHAR(15),
+	email VARCHAR(50)	
+);
